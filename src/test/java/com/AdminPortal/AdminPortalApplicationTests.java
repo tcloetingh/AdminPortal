848760a15f94a2459bcfbd16b90d83b0e513eebc
tcloetingh <tcloetingh@gmail.com>
2022-12-28 21:45:44 -0500

    spring setup

---
+++ src/test/java/com/AdminPortal/AdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdminPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
